563defa91376499779a09736bc20f64a049fc3fc
jeanoza <choikb916@gmail.com>
2021-09-22 16:24:11 +0200

    feat:ex22

--- ex22.js
+++ ex22.js
@@ -7,29 +7,33 @@ function splice(
   let result = "";
   start = start < 0 ? str.length + start : start;
 
-  console.log(
-    "start: ",
-    start,
-    "length: ",
-    str.length,
-    "deleteCount :",
-    deleteCountopt,
-    "toAddopt :",
-    toAddopt
-  );
-  for (let i = 0; i < str.length && deleteCountopt; ++i) {
+  for (let i = 0; i < str.length; ++i) {
     if (toAddopt.length > 0 && i === start) {
       result += toAddopt;
     }
-    if (i > start) {
+    if (i >= start && deleteCountopt > 0) {
       --deleteCountopt;
     } else {
-      result += str[start + i];
+      result += str[i];
     }
+    // console.log(
+    //   "start: ",
+    //   start,
+    //   "length: ",
+    //   str.length,
+    //   "deleteCount :",
+    //   deleteCountopt,
+    //   "toAddopt :",
+    //   toAddopt,
+    //   "index :",
+    //   i,
+    //   "result: ",
+    //   result
+    // );
   }
   return result;
 }
 
-console.log(splice("Tony Tony Ch0pper", 0, 10));
-console.log(splice("Tony Ch0pper", 0, 4, "Tony Tony"));
-console.log(splice("Tony Tony Usopp", -5, 5, "Ch0pper"));
+console.log(1, splice("Tony Tony Ch0pper", 0, 10));
+console.log(2, splice("Tony Ch0pper", 0, 4, "Tony Tony"));
+console.log(3, splice("Tony Tony Usopp", -5, 5, "Ch0pper"));
